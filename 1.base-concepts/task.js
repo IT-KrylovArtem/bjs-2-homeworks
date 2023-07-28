e015ebeb859df911389bfaa7464380109a55bb4e
Krylov Artem <krylov_artem68@mailr.ru>
2023-07-28 07:42:39 +0300

    Первое задание готово

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,34 @@
-"use strict"
+"use strict";
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
+let arr = [];
+const discriminant = b**2-4*a*c;
+  if (discriminant < 0) {
   return arr;
+} else if (discriminant === 0) {
+  const root = -b/(2*a);
+  arr.push(root);
+} else if (discriminant > 0) {
+  const root1 = (-b + Math.sqrt(discriminant) )/(2*a);
+  const root2 = (-b - Math.sqrt(discriminant) )/(2*a);
+  arr.push(root1, root2);
+}  
+return arr;
+}
+function calculateTotalMortgage(percent, contribution, amount, countMonths) { 
+  const monthRate = (percent / 100) / 12;
+  const loanBody = amount - contribution;
+  const monthlyPaymentFormula = loanBody * (monthRate + (monthRate / (((1 + monthRate) ** countMonths) - 1)));
+  const totalAmount = monthlyPaymentFormula * countMonths;   
+  const roundingUpToTwo = Number(totalAmount.toFixed(2));
+return percent, contribution, amount, countMonths, roundingUpToTwo;  
 }
 
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+  let result = calculateTotalMortgage(10, 0, 50000, 12);
+  let result1 = calculateTotalMortgage(10, 1000, 50000, 12);
+  let result2 = calculateTotalMortgage(10, 0, 20000, 24);
+  let result3 = calculateTotalMortgage(10, 1000, 20000, 24);
+  let result4 = calculateTotalMortgage(10, 20000, 20000, 24);
+  let result5 = calculateTotalMortgage(10, 0, 10000, 36);
+  let result6 = calculateTotalMortgage(15, 0, 10000, 36);
   
-}
